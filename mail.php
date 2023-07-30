46363a9f565d1d07a6420a74e45859603f312fe9
Sajjad <haq.sajjad220@gmail.com>
2023-07-30 00:56:42 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "info@yourmail.com";
+        $recipient = "haq.sajjad220@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
